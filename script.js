5716a3694e1a9c50a2afebf409e43437a49166c2
Maru <mstalmach11@gmail.com>
2024-10-28 19:47:37 +0100

    update

--- script.js
+++ script.js
@@ -27,3 +27,28 @@ Clock();
 Today();
 
 setInterval(Clock, 1000);
+
+// fullscreen images
+document.addEventListener("DOMContentLoaded", function () {
+  const galleryImages = document.querySelectorAll(".gallery-pic");
+
+  galleryImages.forEach((img) => {
+    img.addEventListener("click", () => {
+      // Request fullscreen and handle browser-specific prefixes
+      if (img.requestFullscreen) {
+        img.requestFullscreen().catch((err) => console.log(err));
+      } else if (img.webkitRequestFullscreen) {
+        img.webkitRequestFullscreen();
+      } else if (img.msRequestFullscreen) {
+        img.msRequestFullscreen();
+      }
+    });
+  });
+
+  document.addEventListener("fullscreenchange", () => {
+    // Remove 'fullscreen-active' if no element is in fullscreen
+    if (!document.fullscreenElement) {
+      document.body.classList.remove("fullscreen-active");
+    }
+  });
+});
